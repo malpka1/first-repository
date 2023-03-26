8076afda54fb4fd5cd7eb603604c2dae5965bbec
malpka1 <veranika.ivanouskaya@gmail.com>
2023-03-26 18:50:13 +0200

    init commit

---
+++ les1.py
@@ -0,0 +1,21 @@
+>>> (17/2)*3+2
+27.5
+>>> 2+(17/2)*3
+27.5
+>>> 19%4+(15/2)*3
+25.5
+>>> (15+6)-(10*4)
+-19
+>>> (17/2)%2*3**3
+13.5
+>>> str_1="abcdefghi".capitalize()
+>>> print (str_1)
+Abcdefghi
+>>> str_2="abcdefghi".upper()
+>>> print (str_2)
+ABCDEFGHI
+>>> str_3= "abcdefghi" [0:3]
+>>> print (str_3)
+abc
+>>> str_4= "abcdefghi" [1:-1:2]
+>>> print(str_4)
